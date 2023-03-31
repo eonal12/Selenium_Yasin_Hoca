a1ee8eeee47e6a1da7fd3cbb7c4d3ec3faa2f2af
Emre QA <emreonal12@gmail.com>
2023-03-31 08:57:37 +0300

    ...

--- src/test/java/day07/Odev_Radio_Button.java
+++ src/test/java/day07/Odev_Radio_Button.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -29,6 +30,10 @@ public class Odev_Radio_Button {
     @Test
     public void radioButton() {
         driver.get("https://www.facebook.com");
-        driver.findElement(By.xpath("//a[@id='u_0_0_er']")).click();
+        driver.findElement(By.xpath("(//a[@role='button'])[2]")).click();
+        WebElement radioButton = driver.findElement(By.xpath("(//input[@type='radio'])[2]"));
+        if (!radioButton.isSelected()){
+            radioButton.click();
+        }
     }
 }
